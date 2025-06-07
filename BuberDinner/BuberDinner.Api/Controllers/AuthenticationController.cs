ed35f52a009b3239c712dcb2b32ebae102cf7f4b
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-06-07 18:10:20 +0530

    Part 1 - Authentication Service in the Application Layer

--- BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using BuberDinner.Contracts.Authentication;
+﻿using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,19 +9,43 @@ namespace BuberDinner.Api.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
 
         [Route("register")]
         [HttpPost]
         public IActionResult Register(RegisterRequest request)
         {
-            return Ok();
+            var authResult = _authenticationService.Register(request.FirstName, request.LastName, request.Email, request.Password);
+
+            var response = new AuthenticationResponse(
+                authResult.Id,
+                authResult.FirstName,
+                authResult.LastName,
+                authResult.Email,
+                authResult.Token);
+
+            return Ok(response);
         }
 
         [Route("login")]
         [HttpPost]
         public IActionResult Login(LoginRequest request)
         {
-            return Ok();
+            var authResult = _authenticationService.Login(request.Email, request.Password);
+
+            var response = new AuthenticationResponse(
+                authResult.Id,
+                authResult.FirstName,
+                authResult.LastName,
+                authResult.Email,
+                authResult.Token);
+
+            return Ok(response);
         }
 
     }
